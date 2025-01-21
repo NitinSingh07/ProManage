b0c262f7d1423d2c88eb5001f1cabda4dcae0a09
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-21 22:15:34 +0530

    feat: add postUser endpoint for user creation in userController

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -11,3 +11,26 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: `Error fetching users : ${error}` });
   }
 };
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      username,
+      cognitoId,
+      profilePictureUrl = "/i1.jpg",
+      teamId = 1,
+    } = req.body;
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId,
+      },
+    });
+    res.status(200).json({ message: "User created successfully", newUser });
+  } catch (error: any) {
+    res.status(500).json({ message: `Error fetching users : ${error}` });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
@@ -32,6 +33,8 @@ app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
+
+
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
